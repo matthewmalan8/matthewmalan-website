e822b1f84c3b28ec3b82c259866e785e875fa225
Matthew Malan <mattasu6@gmail.com>
2026-05-08 22:30:44 -0700

    Blog

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,31 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="preload"
-          href="/fonts/space-grotesk-variable.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TGMZJS95HP"
         />
-        <link
-          rel="preload"
-          href="/fonts/inter-400.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TGMZJS95HP', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
         />
-        <link
-          rel="preload"
-          href="/fonts/inter-500.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
